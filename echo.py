c7fd1cfb8440a048a102672b323b1d2dea0fd97a
sshannin <sshannin@3bdb8b66-a1aa-b60c-15db-dd7f7f638a7e>
2010-04-08 17:00:58 +0000

    Testing commit/emails.  Also, included is a file I threw together real quick which attempts to emulate the question-based responses.  My favorite thing to type is 'My house is on fire.'

---
+++ echo.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import sys
+import re
+import random, readline
+
+punc=",.?'\";:()-!"
+
+def qa(msg, dest=sys.stdout):
+    """Write a message and get the response."""
+    dest.write(msg+"\n")
+    return raw_input()
+    
+def niceSub(orig, repl):
+    """Performs a string substitution maintaing case on first letter."""
+    if orig[0].islower():
+        return repl[0].lower() +repl[1:]
+    return repl[0].upper()+repl[1:]
+
+def rpl(msg, needle, sub):
+    """Cleanly replaces all words of 'needle' in msg with 'sub'"""
+    return " ".join([niceSub(x,sub) if x.lower()==needle.lower() else x for x in msg.split(" ")])
+
+def commonsubs(orig):
+    """Perform common word substitutions for responding."""
+    orig=rpl(orig,"my","your")
+    orig=rpl(orig,"i","you")
+    orig=rpl(orig,"me","you")    #ppl like to talk about themselves
+    return orig
+
+def makeResponse(statement):
+    """Takes a statement from the user and forms a prompt out of it."""
+    num_choices=5
+    select=random.random()
+    interval = 1.0/num_choices
+    if select <= interval:
+        if statement[-1] in punc:
+            statement=statement[:-1]
+        return statement+"?  How does that make you feel?"
+    if select <= 2*interval:
+        if statement[-1] in punc:
+            statement = statement[:-1]
+        return statement+"? Go on."
+    if select < 3*interval:
+        return "This subject getting too depressing.  Is there anything else"+\
+            " we can talk about?"
+    if select <=4*interval:
+        return "You bore me, but continue if you must."
+    if select <=5*interval:
+        return "The same was once true for me."
+
+
+def main(args=None):
+    if args is None:
+        args=sys.argv[1:]
+    s=None
+    while s!="quit":
+        if s is None:
+            msg="Hello."
+            s=qa(msg)
+            continue
+        msg=commonsubs(s)
+        s=qa(makeResponse(msg))
+
+if __name__=="__main__":
+    exit(main())
