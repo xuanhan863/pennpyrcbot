fe67d9990cc09d175e1a70465af8fb3934244347
sshannin <sshannin@3bdb8b66-a1aa-b60c-15db-dd7f7f638a7e>
2010-04-19 02:53:39 +0000

    Made some stuff more global.

--- Global.py
+++ Global.py
@@ -21,3 +21,47 @@ advs=cPickle.load(tmp)
 
 class POS:
 	noun, verb, adj, adv, null = range(5)
+	def getPOSFromFiles(word):#shouldn't be needed outside of this code
+		ret = set()
+		if word in nouns:
+			ret.add(POS.noun)
+		if word in verbs:
+			ret.add(POS.verb)
+		if word in adjs:
+			ret.add(POS.adj)
+		if word in advs:
+			ret.add(POS.adv)
+		if len(ret) == 0:
+			ret.add(POS.null)
+		return ret
+
+###########build our map of words -> set of POS
+if not os.path.exists("posmap.bin"):
+	POSMap = {}
+	for word in nouns:
+		POSMap[word] =  POS.getPOSFromFiles(word)
+	for word in adjs:
+		POSMap[word] = POS.getPOSFromFiles(word)
+	for word in verbs:
+		POSMap[word] = getPOSFromFiles(word)
+	for word in advs:
+		POSMap[word] = getPOSFromFiles(word)
+	dest=open("posmap.bin","wb")
+	cPickle.dump(POSMap,dest,protocol=2)
+	dest.close()
+else:
+	source=open("posmap.bin","rw")
+	POSMap=cPickle.load(source)
+	source.close()
+#####dict is built
+def getPOS(word):
+    try:
+        return POSMap[word]
+    except KeyError:
+        pass
+    if word[-1]=="s":
+        try:
+            return POSMap[word[:-1]]
+        except KeyError:
+            pass
+    return set([POS.null])
--- sentcheck.py
+++ sentcheck.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import sys, cPickle
-from Global import nouns, adjs, advs, verbs, POS
+from Global import nouns, adjs, advs, verbs, POS, getPOS
 
 validFormats=[POS.null,POS.adj, POS.noun, POS.adv, POS.verb, POS.null, POS.adj, POS.noun]
 
@@ -13,31 +13,19 @@ def main(args=None):
         inList = inLine.lower()[:-1].split(" ")
         for word in inList:
             posDict[word] = getPOS(word)
-#            print "Added '%s' with set: %s"%(word,posDict[word])
-    
+    #        print "Added '%s' with set: %s"%(word,posDict[word])
         for i in range(len(inList)):
- #           print "Working on word: '%s' with index %s"%(inList[i],i)
+     #       print "Working on word: '%s' with index %s"%(inList[i],i)
             if validFormats[i] not in posDict[inList[i]]:
                 print "Did not match structure on",inLine
-                break
- #           if i == len(inList)-1:
-#                print "Good structure on", inLine
-        
-        inLine = raw_input()
-
-def getPOS(word):
-    ret = set()
-    if word in nouns or (word[-1]=="s" and word[:-1] in nouns):
-        ret.add(POS.noun)
-    if word in verbs or (word[-1]=="s" and word[:-1] in verbs):
-        ret.add(POS.verb)
-    if word in adjs:
-        ret.add(POS.adj)
-    if word in advs:
-        ret.add(POS.adv)
-    if len(ret) == 0:
-        ret.add(POS.null)
-    return ret
+                return 1
+       #     if i == len(inList)-1:
+      #          print "Good structure on", inLine
+        try:
+            inLine = raw_input()
+        except EOFError:
+            print "All tests passed!"
+            return 0
 
 if __name__ == "__main__":
     exit(main())
