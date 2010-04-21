521869ccca0a9ee2e015ef5cf308c7536735755f
sshannin <sshannin@3bdb8b66-a1aa-b60c-15db-dd7f7f638a7e>
2010-04-21 05:40:51 +0000

    Large changes to storage/data loading structure. Strongly attempting to keep global interface uncluttered. Implemented category matching as well.

--- Global.py
+++ Global.py
@@ -2,45 +2,16 @@ import os, sys, cPickle, defs
 
 execfile("startup.py") ##make sure all of our bin files are generated.
 
-def lookup(str):
-	"""Returns a Word object for the given string.  This is the interface
-that should be used to "lookup" a word in the bot's knowledge base."""
-	return Word(str)
-
 POS=defs.POS
+Category=defs.Category #deal with this later
+Word = defs.Word
+Noun = defs.Noun
+Adj = defs.Adj
+Adv = defs.Adv
+Det = defs.Det
 
-class Word:
-	POS=POS.null;
-	def __init__(self, str):
-		self.val=str
-	pass
-class Noun(Word):
-	POS=POS.noun
-	def __init__(self,str):
-		Word.__init__(self.str)
-	pass
-class Adj(Word):
-	POS=POS.adj
-	def __init__(self,str):
-		Word.__init__(self.str)
-	pass
-class Adv(Word):
-	POS=POS.adv
-	def __init__(self,str):
-		Word.__init__(self.str)
-	pass
-class Verb(Word):
-	POS=POS.verb
-	def __init__(self,str):
-		Word.__init__(self.str)
-	pass
-class Det(Word):
-	POS=POS.det
-	def __init__(self,str):
-		Word.__init__(self.str)
-	pass
-nouns=defs.loadFile("noun.bin") #now load the binaries
-adjs=defs.loadFile("adjective.bin")
-verbs=defs.loadFile("verb.bin")
-advs=defs.loadFile("adverb.bin")
-dets=defs.loadFile("determiners.bin")
+words=defs.loadFile("masterwordtable.bin")
+#cats = defs.loadFile("cat.bin") for later
+def lookup(str):
+	"""Returns a Word object for the given string.  This is the interface that should be used to "lookup" a word in the bot's knowledge base."""
+	return words[str]
--- startup.py
+++ startup.py
@@ -1,37 +1,52 @@
 import os, sys, cPickle, defs
-from defs import outOfDate, loadFile, POS
+from defs import outOfDate, loadFile, POS, Category
 
+wordListSources = { "noun.dat" : defs.Noun, "adjective.dat" : defs.Adj, 
+              "verb.dat" : defs.Verb, "adverb.dat" : defs.Adv, 
+              "determiners.dat" : defs.Det}
+binWordLists = ["noun.bin", "adjective.bin", "verb.bin", "adverb.bin", 
+                "determiners.bin"]
+wordtablefname = "masterwordtable.bin"
+categorytablefname = "categorytable.bin"
 #########build our original word lists#############
-for fname in os.listdir("."): #first, create the binaries/verify presence
-    if fname[-4:]==".dat" and outOfDate(fname[:-3]+"bin", [fname]):
-                source=open(fname)
-                dest=open(fname[:-3]+"bin","wb")
-                ls = [line.strip() for line in source]
-                cPickle.dump(ls,dest,protocol=2)
-                print "Wrote %s"%(fname[:-3]+"bin",)
-                source.close()
-                dest.close()
+for fname in wordListSources: #first, ensure the binaries exist
+    if outOfDate(fname[:-3]+"bin", [fname]):
+        factory = wordListSources[fname]
+        ####parse each line and add to list
+        source=open(fname)
+        ls = [factory(line.strip(),"") if len(line.strip().split(" ")) == 1 \
+                  else factory(*line.strip().split(" ")) for line in source]
+        source.close()
+        ####add to master list
+        ####and now dump the list to hdd
+        dest=open(fname[:-3]+"bin","wb")
+        cPickle.dump(ls,dest,protocol=2)
+        dest.close()
+        del ls
+        print "Wrote %s"%(fname[:-3]+"bin",)
 ########word lists are now built##########
-
-###########build our map of words -> POS######
-deps=[]
-for dep in os.listdir("."):
-	if dep[-4:] == ".dat":
-		deps.append(dep)
-if outOfDate("posmap.bin",deps):
-	POSMap = {}
-	for word in loadFile("noun.bin"):
-		POSMap[word] =  POS.noun
-	for word in loadFile("adjective.bin"):
-		POSMap[word] = POS.adj
-	for word in loadFile("verb.bin"):
-		POSMap[word] = POS.verb
-	for word in loadFile("adverb.bin"):
-		POSMap[word] = POS.verb
-	for word in loadFile("determiners.bin"):
-		POSMap[word] = POS.det
-	dest=open("posmap.bin","wb")
-	cPickle.dump(POSMap,dest,protocol=2)
-	dest.close()
-	print "Wrote dictionary."
+allwords = {}
+###### now we export our master lookup list######
+if outOfDate(wordtablefname,binWordLists):
+    for file in binWordLists:
+        ls = loadFile(file)
+        for word in ls:
+            allwords[word.val]=word
+        del ls
+    dest=open(wordtablefname,"wb")
+    cPickle.dump(allwords,dest,protocol=2)
+    dest.close()
+    print "Just wrote out the master word table."
+else:
+    allwords = loadFile(wordtablefname)  #we need this loaded for next step
 #########dict is built##########
+######now we build the category table#####
+cats={}
+source = open("cat.dat")
+#for line in source:
+#    c = Category.lineToCat(line, cats.__getitem__, allwords.__getitem__)
+#    cats[c.tag] = c  #built on line above.  now we insert
+#dest=open("cat.bin","rw")
+#cPickle.dump(cats,dest,protocol=2)
+#dest.close()   ##########for later#########
+#print "Just dumped the category table."
