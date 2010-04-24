b8489abc8ce3af200bb1d766eb37b469fa827927
gianni245 <gianni245@3bdb8b66-a1aa-b60c-15db-dd7f7f638a7e>
2010-04-24 00:04:08 +0000

    prep work done, responder unimplemented, food break, brb ;)

--- responder.py
+++ responder.py
@@ -1,26 +1,69 @@
 #!/usr/bin/python
 
-import sys, re, categorizer
+"""
+Input Format:
+for word = var
+
+type:word theme:word@thm:word@det:word@des:word@cor agent:word@agt:word@det:word@des:word@cor
+"""
+import sys, re, categorizer, random
 
 def main(args=None):
-    #makes map word:POS
-    taggedString = "happy@adj book@noun horse@noun say@vrb"
-    map = {}
-    taggedList = taggedString.split(" ")
-    for element in taggedList:
-        pair = element.split("@")
-        map[pair[1]] = pair[0]
-    for k in map:
-        print k,map[k]
+    #makes maps
+    theme = {}
+    agent = {}
+    type = ""
+    input = "type:word theme:word@thm:word@det:word@des:word@cor agent:word@agt:word@det:word@des:word@cor"
+
+    #sectionList breakdown
+    #type[]
+    #theme[]
+    #agent[]
+    sectionList = input.split(" ")
+    
+    for group in sectionList:
+        currentDict = {}
+        groupList=group.split(":")
+        if groupList[0] == type:
+            type = groupList[1]
+        elif groupList[0] == "theme" or groupList[0] = "agent":
+            for element in groupList:
+                pair = element.split("@")
+                currentDict[pair[1]] = pair[0]
+        else:
+            print "Input format not recognized"
+
+        if groupList[0] == "theme":
+            theme = currentDict
+        elif groupList[0] == "agent":
+            agent = currentDict
+
+    #generate response
+    genResponse(type, theme, agent)
 
 #generates response from word:POS using category
-def genResponse(map):
-    #find common word in each POS map
+def genResponse(type, tMap, aMap):
+    #find common topic for each
+    commons = getCommons(tMap[thm])
 
     #change sentence structure
 
     #construct sentence using 1 or 2 of common words
 
+def getCommons(theme):
+    catFile = open(cat.dat, "r")
+    ret = ["@action", "@agent", "@desc"]
+    for line in catFile:
+        lineList = line.split(" ")
+        if theme == lineList[0]:
+            acList = lineList[2].split(":")
+            ret[0] = acList[random.randint(1,len(acList)-1)]
+            agList = lineList[3].split(":")
+            ret[1] = agList[random.randint(1,len(agList)-1)]
+            deList = lineList[4].split(":")
+            ret[2] = deList[random.randint(1,len(deList)-1)]
+            return ret
+    return None
 
 if __name__=="__main__":
     exit(main())
