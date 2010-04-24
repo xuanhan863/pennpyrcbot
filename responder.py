939dc414b29e57259100f8c59cf4324c1cb1a993
gianni245 <gianni245@3bdb8b66-a1aa-b60c-15db-dd7f7f638a7e>
2010-04-24 02:24:14 +0000

    one type of response is working, but input is broken enough to flexibly generate alternate responses.

--- responder.py
+++ responder.py
@@ -13,7 +13,10 @@ def main(args=None):
     theme = {}
     agent = {}
     type = ""
-    input = "type:word theme:word@thm:word@det:word@des:word@cor agent:word@agt:word@det:word@des:word@cor"
+    input2 = "type:word theme:word@thm:word@det:word@des:word@cor agent:word@agt:word@det:word@des:word@cor"
+
+    #sample input is currently rotten carrots
+    input = "type:statement theme:veg@thm:the@det:rotten@des:carrot@cor agent:carrot@agt:the@det:rotten@des:carrot@cor"
 
     #sectionList breakdown
     #type[]
@@ -24,10 +27,10 @@ def main(args=None):
     for group in sectionList:
         currentDict = {}
         groupList=group.split(":")
-        if groupList[0] == type:
+        if groupList[0] == "type":
             type = groupList[1]
-        elif groupList[0] == "theme" or groupList[0] = "agent":
-            for element in groupList:
+        elif groupList[0] == "theme" or groupList[0] == "agent":
+            for element in groupList[1:]:
                 pair = element.split("@")
                 currentDict[pair[1]] = pair[0]
         else:
@@ -39,19 +42,21 @@ def main(args=None):
             agent = currentDict
 
     #generate response
-    genResponse(type, theme, agent)
+    print genResponse(type, theme, agent)
 
 #generates response from word:POS using category
 def genResponse(type, tMap, aMap):
-    #find common topic for each
-    commons = getCommons(tMap[thm])
+    #find alternate common topic for each through rgen
+    #reminder: commons is a list [action, agent, descriptor]
+    commons = getCommons(tMap["thm"])
 
-    #change sentence structure
-
-    #construct sentence using 1 or 2 of common words
+    #construct sentence
+    #yet to implement probabilistic approach to choosing answers
+    #goodbye func can be called at a low probability
+    return question(commons)
 
 def getCommons(theme):
-    catFile = open(cat.dat, "r")
+    catFile = open("cat.dat", "r")
     ret = ["@action", "@agent", "@desc"]
     for line in catFile:
         lineList = line.split(" ")
@@ -64,6 +69,21 @@ def getCommons(theme):
             ret[2] = deList[random.randint(1,len(deList)-1)]
             return ret
     return None
+
+def question(commons):
+    print "Would you do the same with " + commons[2] + " " + commons[1] + "?"
+
+def comment(commons):
+    pass
+
+def imperative(commons):
+    pass
+
+def generalize():
+    pass
+
+def goodbye():
+    pass
 
 if __name__=="__main__":
     exit(main())
